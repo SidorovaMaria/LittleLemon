111a942aac479dc6a159d59d186526e5a3819677
maria sidorova <sidmashav@icloud.com>
2024-10-16 15:31:24 +0100

    Prepared for serach engines,ready to add components"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { About } from "./components/About";
+import { Footer } from "./components/Footer";
+import { HeroSection } from "./components/HeroSection";
+import { Highlights } from "./components/Highlights";
+import { NavigationBar } from "./components/NavigationBar";
+import { Testimonials } from "./components/Testimonials";
 
 function App() {
-  return <div className="App">HomePage</div>;
+  return (
+    <>
+      <nav>
+        <NavigationBar />
+      </nav>
+      <main>
+        <HeroSection />
+        <Highlights />
+        <Testimonials />
+        <About />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default App;
